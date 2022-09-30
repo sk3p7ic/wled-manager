15233c38af448f9ec0eb14f758c33ba5949716ed
Joshua Ibrom <joshuaibrom@gmail.com>
2022-09-30 17:48:37 -0500

    Added form to create new devices

---
+++ pages/api/devices/new.ts
@@ -0,0 +1,13 @@
+import { NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+
+const handle = async (req: any, res: NextApiResponse) => {
+  const { ip, name } = req.body;
+  console.log(ip, name);
+  await prisma.device.create({
+    data: { ip: ip, name: name, locationX: 50, locationY: 50 },
+  });
+  res.redirect("/");
+};
+
+export default handle;
--- pages/newDevice.tsx
+++ pages/newDevice.tsx
@@ -1,7 +1,64 @@
 import type { NextPage } from "next";
+import { FormEvent, useState } from "react";
 
 const NewDevicePage: NextPage = () => {
-  return <div>New Device</div>;
+  const [nameInputValue, setNameInputValue] = useState("");
+  const [ipInputValue, setIpInputValue] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await fetch("/api/devices/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ip: ipInputValue, name: nameInputValue }),
+    });
+    document.location.replace("/"); // Go back home
+  };
+
+  return (
+    <div className="pt-12 flex-grow flex flex-col items-center gap-4">
+      <h1 className="text-5xl border-b border-b-neutral-100">
+        Add a New Device
+      </h1>
+      <form
+        className="grid grid-cols-2 gap-2"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <label htmlFor="device-name-input" className="text-xl">
+          Name:
+        </label>
+        <input
+          id="device-name-input"
+          className="px-2 text-lg text-neutral-800"
+          placeholder="Device name"
+          value={nameInputValue}
+          onChange={(e) => setNameInputValue(e.target.value)}
+        />
+        <label htmlFor="device-ip-input" className="text-xl">
+          IP Address:
+        </label>
+        <input
+          id="device-ip-input"
+          className="px-2 text-lg text-neutral-800"
+          placeholder="#.#.#.#"
+          value={ipInputValue}
+          onChange={(e) => setIpInputValue(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="col-span-2 py-2 text-xl bg-neutral-900 hover:bg-emerald-600 transition-all"
+        >
+          Add Device
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export const getStaticProps = () => {
+  return { props: {} };
 };
 
 export default NewDevicePage;
