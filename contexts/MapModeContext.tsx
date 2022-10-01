ab892071c66871ef0a3a04e79f0440b49499b8fc
Joshua Ibrom <joshuaibrom@gmail.com>
2022-09-30 20:23:32 -0500

    Added more state

--- contexts/MapModeContext.tsx
+++ contexts/MapModeContext.tsx
@@ -3,6 +3,9 @@ import React, { ReactNode, useContext, useState } from "react";
 const MapModeContext = React.createContext({
   editMode: false,
   toggle: () => {},
+  didPosChange: false,
+  setPosChange: () => {},
+  reset: () => {},
 });
 
 export const useMapMode = () => useContext(MapModeContext);
@@ -15,14 +18,33 @@ export const MapModeContextProvider = ({
   children,
 }: MapModeContextProviderProps) => {
   const [editMode, setEditMode] = useState(false);
+  const [didPosChange, setPosChange] = useState(false);
 
   const toggle = () => {
     const curr = editMode;
     setEditMode(!curr);
   };
 
+  const setPosChangeMethod = () => {
+    const curr = didPosChange;
+    setPosChange(!curr);
+  };
+
+  const reset = () => {
+    setEditMode(false);
+    setPosChange(false);
+  };
+
   return (
-    <MapModeContext.Provider value={{ editMode, toggle }}>
+    <MapModeContext.Provider
+      value={{
+        editMode,
+        toggle,
+        didPosChange,
+        setPosChange: setPosChangeMethod,
+        reset,
+      }}
+    >
       {children}
     </MapModeContext.Provider>
   );
