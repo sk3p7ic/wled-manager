20a1ad59f927a789ab2826ac929f6c9cbd948897
Joshua Ibrom <joshuaibrom@gmail.com>
2022-09-30 20:54:06 -0500

    Made nodes draggable

--- components/LocationMap/MapDisplay.tsx
+++ components/LocationMap/MapDisplay.tsx
@@ -1,4 +1,5 @@
 import { Device } from "@prisma/client";
+import React, { useEffect, useState } from "react";
 import { useMapMode } from "../../contexts/MapModeContext";
 import { MapDisplayNode } from "./MapDisplayNode";
 
@@ -7,12 +8,28 @@ type MapDisplayProps = {
 };
 
 export const MapDisplay = ({ nodes }: MapDisplayProps) => {
-  const { editMode, didPosChange, setPosChange } = useMapMode();
+  const { editMode, setPosChange } = useMapMode();
+  const mapRef = React.createRef<HTMLDivElement>();
+  const [dimen, setDimen] = useState({ w: 0, h: 0 });
+
+  useEffect(() => {
+    if (mapRef.current)
+      setDimen({
+        w: mapRef.current.clientWidth,
+        h: mapRef.current.clientHeight,
+      });
+  }, [mapRef]);
 
   return (
-    <div className="flex-grow relative">
+    <div className="flex-grow relative" ref={mapRef}>
       {nodes.map((node) => (
-        <MapDisplayNode node={node} key={node.id} draggable={editMode} />
+        <MapDisplayNode
+          node={node}
+          key={node.id}
+          draggable={editMode}
+          onPosChange={setPosChange}
+          mapSize={dimen}
+        />
       ))}
     </div>
   );
--- components/LocationMap/MapDisplayNode.tsx
+++ components/LocationMap/MapDisplayNode.tsx
@@ -1,12 +1,19 @@
 import { Device } from "@prisma/client";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelectedMapDevice } from "../../contexts/SelectedMapDevice";
 
 type MapDisplayNodeProps = {
   node: Device;
   draggable: boolean;
+  onPosChange: () => void;
+  mapSize: { w: number; h: number };
 };
-export const MapDisplayNode = ({ node, draggable }: MapDisplayNodeProps) => {
+export const MapDisplayNode = ({
+  node,
+  draggable,
+  onPosChange,
+  mapSize,
+}: MapDisplayNodeProps) => {
   const [coords, setCoords] = useState<{ x: number; y: number }>({
     x: node.locationX,
     y: node.locationY,
@@ -25,6 +32,72 @@ export const MapDisplayNode = ({ node, draggable }: MapDisplayNodeProps) => {
 
   const { device, setDevice } = useSelectedMapDevice();
 
+  const validateMaxDimen = (
+    top: number,
+    left: number,
+    width: number,
+    height: number
+  ): Boolean => {
+    const dimen = mapSize;
+    return top + height <= dimen.h && left + width <= dimen.w;
+  };
+
+  const enableDragging = (element: HTMLElement) => {
+    let prevX = 0;
+    let prevY = 0;
+    element.onmousedown = (me: MouseEvent) => {
+      me.preventDefault();
+      prevX = me.clientX;
+      prevY = me.clientY;
+      document.onmouseup = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+      };
+      document.onmousemove = (e: MouseEvent) => {
+        e.preventDefault();
+        const newTop = element.offsetTop - (prevY - e.clientY);
+        const newLeft = element.offsetLeft - (prevX - e.clientX);
+        if (
+          newTop >= 0 &&
+          newLeft >= element.clientWidth / 2 &&
+          validateMaxDimen(
+            newTop,
+            newLeft,
+            (element.clientWidth / 4) * 3,
+            element.clientHeight
+          )
+        ) {
+          element.style.top = newTop + "px";
+          element.style.left = newLeft + "px";
+        }
+        prevX = e.clientX;
+        prevY = e.clientY;
+        const coords = toPercentCoords(element);
+        if (coords.x !== node.locationX && coords.y !== node.locationY)
+          onPosChange();
+      };
+    };
+  };
+
+  type CoordsType = {
+    x: number;
+    y: number;
+  };
+
+  const toPercentCoords = (e: HTMLElement): CoordsType => {
+    const x = (Number.parseFloat(e.style.left) / mapSize.w) * 100;
+    const y = (Number.parseFloat(e.style.top) / mapSize.h) * 100;
+    return { x, y };
+  };
+
+  const nodeRef = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (!nodeRef.current) return;
+    enableDragging(nodeRef.current);
+    // eslint-disable-next-line
+  }, [draggable]);
+
   return (
     <div
       className={`absolute h-12 w-12 rounded-lg text-sm flex items-center justify-center cursor-pointer ${
@@ -32,6 +105,7 @@ export const MapDisplayNode = ({ node, draggable }: MapDisplayNodeProps) => {
       } ${status ? "bg-emerald-600" : "bg-rose-600"}`}
       style={{ top: `${coords.y}%`, left: `${coords.x}%` }}
       onClick={() => setDevice(node)}
+      ref={nodeRef}
     >
       {node.id}
     </div>
